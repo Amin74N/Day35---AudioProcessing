6ae036f768e4ada479e2d3e79a7f539128e2a6e4
Amin <aminnovin@gmail.com>
2026-07-31 23:45:33 +0330

    Implementing a CNN for Sound Classification

---
+++ 03-Sound_Classification_CNN.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+from torchsummary import summary
+# ----------------------------------------------------------------------------------------------------------------------
+# Building a Convolutional Neural Network (CNN) Model:
+# 4 convolution blocks (conv + relu + maxpool) -> flatten -> linear -> softmax:
+class CNNNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=2)
+                                   , nn.ReLU()
+                                   , nn.MaxPool2d(kernel_size=2))
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=2)
+                                   , nn.ReLU()
+                                   , nn.MaxPool2d(kernel_size=2))
+        self.conv3 = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=2)
+                                   , nn.ReLU()
+                                   , nn.MaxPool2d(kernel_size=2))
+        self.conv4 = nn.Sequential(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=2)
+                                   , nn.ReLU()
+                                   , nn.MaxPool2d(kernel_size=2))
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(in_features=128*5*4, out_features=10)
+        self.softmax = nn.LogSoftmax(dim=1)
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        logits = self.linear(x)
+        predictions = self.softmax(logits)
+        return predictions
+model = CNNNetwork()
+summary (model, (1,64,44))
